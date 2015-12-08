9ea8ca60e2927a871f2243ecbd62731f0ae694b4
Jesús <jesus.vallgo@gmail.com>
2015-12-08 16:16:19 -0600

    Avance

--- app/src/main/java/cenapred/gob/mx/holamundo/MainActivity.java
+++ app/src/main/java/cenapred/gob/mx/holamundo/MainActivity.java
@@ -5,11 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.app.ActionBar;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private TextView contenido;
+    private TextView texto;
+    private EditText editText;
+    private ImageView imgMain;
+    private Button btnMain,btnImagen,btnTexto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +28,20 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        contenido = (TextView)findViewById(R.id.txtSaludo);
+        texto = (TextView)findViewById(R.id.txtNombre);
+        editText = (EditText)findViewById(R.id.editText);
+        imgMain = (ImageView)findViewById(R.id.imgMain);
+
+        editText.setHint(editText.getHint() + " " + texto.getText() + " " + imgMain.getScaleType());
+
+        btnMain = (Button)findViewById(R.id.btnMain);
+        btnMain.setOnClickListener(this);
+
+        btnImagen = (Button)findViewById(R.id.btnImagen);
+        btnImagen.setOnClickListener(this);
+
+        btnTexto = (Button)findViewById(R.id.btnTxv);
+        btnTexto.setOnClickListener(this);
     }
 
     @Override
@@ -31,23 +53,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
-            case R.id.add:
-                contenido.setText("metodo para agregar");
-                return true;
-            case R.id.search:
-                contenido.setText("metodo para buscar");
-                return true;
-            case R.id.edit:
-                contenido.setText("metodo para editar");
-                return true;
-            case R.id.delete:
-                contenido.setText("metodo para borrar");
-                return true;
-            case R.id.action_settings:
-                contenido.setText("metodo para opciones");
+            case R.id.accion1:
+                contenido.setText("metodo para accion 1");
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnMain:
+                Toast.makeText(getApplicationContext(),"Texto de prueba para toast",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.btnTxv:
+                editText.setText("Cambiado desde aplicacion");
+                break;
+            case R.id.btnImagen:
+                imgMain.setImageResource(R.drawable.prueba);
+                break;
+            default:
+                break;
         }
     }
 }
